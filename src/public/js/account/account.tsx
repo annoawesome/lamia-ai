6799a25aa35e10f07b4f26b59f53d3a6733c64f8
Seth Hoong <hoongseth@gmail.com>
2025-08-27 20:03:18 -0400

    Import styles in Account page

--- src/public/js/account/account.tsx
+++ src/public/js/account/account.tsx
@@ -2,6 +2,8 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './AccountPage';
+import '../../css/style.css';
+import '../../css/account.css';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
