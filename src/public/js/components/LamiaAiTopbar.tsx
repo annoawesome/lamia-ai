9e810724801bac017469876df8d7e50ae292fe28
Seth Hoong <hoongseth@gmail.com>
2025-10-02 21:46:20 -0400

    Fix topbar not showing logo

--- src/public/js/components/LamiaAiTopbar.tsx
+++ src/public/js/components/LamiaAiTopbar.tsx
@@ -1,9 +1,10 @@
+import logoUrl from "../../assets/favicon-32x32.png";
 import React from "react";
 
 export default function LamiaAiTopbar() {
     return (
         <nav className="flex-flow-right" id="lamia-topbar">
-            <a className="btn btn-medium flex-flow-right" href="/"><img src="assets/favicon-32x32.png" alt="Lamia AI Logo"></img><h2>LamiaAI</h2></a>
+            <a className="btn btn-medium flex-flow-right" href="/"><img src={logoUrl} alt="Lamia AI Logo"></img><h2>LamiaAI</h2></a>
             <a className="btn btn-tertiary btn-medium" href="/account">Account</a>
             <a className="btn btn-tertiary btn-medium">Pricing</a>
             <a className="btn btn-tertiary btn-medium">Blog</a>
